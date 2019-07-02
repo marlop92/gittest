b644103f0526f850677d530a065e05c69a5e6d14
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-02 09:25:41 +0200

    Add person details

--- src/main/java/pl/mlopatka/model/Person.java
+++ src/main/java/pl/mlopatka/model/Person.java
@@ -4,4 +4,18 @@ package pl.mlopatka.model;
  * Created by marcin.lopatka on 02-07-2019
  */
 public class Person {
+	
+	private String name;
+	
+	public Person(final String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(final String name) {
+		this.name = name;
+	}
 }
