111aa4e0a7fb8f4fc7e78c52b380cf13b63ff5c7
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-02 09:25:00 +0200

    Init commit

--- src/main/java/pl/mlopatka/Main.java
+++ src/main/java/pl/mlopatka/Main.java
@@ -4,4 +4,8 @@ package pl.mlopatka;
  * Created by marcin.lopatka on 02-07-2019
  */
 public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Works!");
+	}
 }
---
+++ src/main/java/pl/mlopatka/model/Person.java
@@ -0,0 +1,7 @@
+package pl.mlopatka.model;
+
+/**
+ * Created by marcin.lopatka on 02-07-2019
+ */
+public class Person {
+}
