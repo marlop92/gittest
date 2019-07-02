c76d5706b9dd260fabedd0e9cf6859160754a0fd
test <marcin.lopatka@mercareon.com>
2019-07-02 12:21:07 +0200

    Enhance Person model

--- src/main/java/pl/mlopatka/model/Person.java
+++ src/main/java/pl/mlopatka/model/Person.java
@@ -6,9 +6,11 @@ package pl.mlopatka.model;
 public class Person {
 	
 	private String name;
+	private int age;
 	
-	public Person(final String name) {
+	public Person(final String name, final int age) {
 		this.name = name;
+		this.age = age;
 	}
 	
 	public String getName() {
@@ -17,5 +19,13 @@ public class Person {
 	
 	public void setName(final String name) {
 		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(final int age) {
+		this.age = age;
 	}
 }
